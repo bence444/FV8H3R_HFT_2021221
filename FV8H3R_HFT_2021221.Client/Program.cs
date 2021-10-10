75385c57050ac4b0872ece423bf1811f158a153e
bence444 <lovasz.b99@gmail.com>
2021-10-10 17:52:08 +0200

    Add project files.

---
+++ FV8H3R_HFT_2021221.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FV8H3R_HFT_2021221.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FV8H3R_HFT_2021221.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FV8H3R_HFT_2021221.Test
+{
+    public class Class1
+    {
+    }
+}
