eda14327693c4076a6e5db8d82a5a13a51305baf
bence444 <lovasz.b99@gmail.com>
2021-10-16 20:47:16 +0200

    data classes struct

---
+++ ABC123_HFT_2021221.Data/TinderDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FV8H3R_HFT_2021221.Data
+{
+    class TinderDBContext : DbContext
+    {
+    }
+}
