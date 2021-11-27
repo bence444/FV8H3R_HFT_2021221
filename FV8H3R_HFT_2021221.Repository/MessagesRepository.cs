130fe17b5117095ac051492a7c2f9fe811994365
bence444 <lovasz.b99@gmail.com>
2021-11-27 18:24:36 +0100

    classes moved into separately files

---
+++ FV8H3R_HFT_2021221.Repository/MessagesRepository.cs
@@ -0,0 +1,42 @@
+﻿using FV8H3R_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace FV8H3R_HFT_2021221.Repository
+{
+    public class MessagesRepository : Repository<Messages>, IMessagesRepository
+    {
+        public MessagesRepository(DbContext ctx) : base(ctx) { }
+
+        public void UnsendMessage(int id)
+        {
+            var msg = GetOne(id);
+
+            if (msg == null)
+                throw new InvalidOperationException("Not found");
+
+            msg.Deleted = true;
+            ctx.SaveChanges();
+        }
+
+        public override void Delete(int id)
+        {
+            ctx.Set<Messages>().Remove(GetOne(id));
+            ctx.SaveChanges();
+        }
+
+        public override Messages GetOne(int id)
+        {
+            return GetAll().SingleOrDefault(x => x.Id == id);
+        }
+
+        public override void Update(Messages updated)
+        {
+            var msgToUpdate = GetOne(updated.Id);
+
+            msgToUpdate.MessagesSent = updated.MessagesSent;
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ FV8H3R_HFT_2021221.Repository/UserRepository.cs
@@ -0,0 +1,44 @@
+﻿using FV8H3R_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace FV8H3R_HFT_2021221.Repository
+{
+    public class UserRepository : Repository<Users>, IUserRepository
+    {
+        public UserRepository(DbContext ctx) : base(ctx) { }
+
+        public void ChangeBio(int id, string text)
+        {
+            var user = GetOne(id);
+
+            if (user == null)
+                throw new InvalidOperationException("No user found");
+
+            user.Bio = text;
+
+            ctx.SaveChanges();
+        }
+
+        public override void Delete(int id)
+        {
+            Delete(GetOne(id));
+        }
+
+        public override Users GetOne(int id)
+        {
+            return GetAll().SingleOrDefault(x => x.Id == id);
+        }
+
+        public override void Update(Users updated)
+        {
+            var userToUpdate = GetOne(updated.Id);
+
+            userToUpdate.Name = updated.Name;
+            userToUpdate.AvailableLikes = updated.AvailableLikes;
+            userToUpdate.RegDate = updated.RegDate;
+            ctx.SaveChanges();
+        }
+    }
+}
