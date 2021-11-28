392b62ffd621689d4a170633fa64ef940c598a9b
bence444 <lovasz.b99@gmail.com>
2021-11-28 22:55:05 +0100

    added logic to messages

---
+++ FV8H3R_HFT_2021221.Logic/IMessagesLogic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FV8H3R_HFT_2021221.Models;
+using FV8H3R_HFT_2021221.Repository;
+
+namespace FV8H3R_HFT_2021221.Logic
+{
+    public interface IMessagesLogic
+    {
+        Messages GetOne(int id);
+        IList<Messages> GetAll();
+        void Update(Messages updated);
+        void Create(Messages newMessage);
+        void Delete(Messages forDelete);
+        void Delete(int id);
+        double AverageLength();
+    }
+}
---
+++ FV8H3R_HFT_2021221.Logic/MessagesLogic.cs
@@ -0,0 +1,55 @@
+﻿using FV8H3R_HFT_2021221.Models;
+using FV8H3R_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FV8H3R_HFT_2021221.Logic
+{
+    public class MessagesLogic : IMessagesLogic
+    {
+        IMessagesRepository msgRepo;
+
+        public MessagesLogic(IMessagesRepository msgRepo)
+        {
+            this.msgRepo = msgRepo;
+        }
+
+        public void Create(Messages newMessage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Messages forDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Messages> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Messages GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Messages updated)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double AverageLength()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
