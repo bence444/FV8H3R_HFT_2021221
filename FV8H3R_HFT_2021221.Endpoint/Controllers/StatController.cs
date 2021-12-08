cd7093d2d7e9ca34f5e8bef09dbee49aba884f29
bence444 <lovasz.b99@gmail.com>
2021-12-08 23:00:45 +0100

    added controllers

---
+++ FV8H3R_HFT_2021221.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FV8H3R_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        // GET: api/<StatController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StatController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StatController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StatController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StatController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
