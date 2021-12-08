82f7f74e9761809130685881a3d6a80045ac91fc
bence444 <lovasz.b99@gmail.com>
2021-12-08 18:02:13 +0100

    added stats logic

---
+++ FV8H3R_HFT_2021221.Logic/StatsLogic.cs
@@ -0,0 +1,24 @@
+﻿using FV8H3R_HFT_2021221.Models;
+using FV8H3R_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FV8H3R_HFT_2021221.Logic
+{
+    public class StatsLogic
+    {
+        IRepository<User> userRepo;
+        IRepository<Match> matchRepo;
+        IRepository<Message> msgRepo;
+
+        public StatsLogic(IRepository<User> userRepo, IRepository<Match> matchRepo, IRepository<Message> msgRepo)
+        {
+            this.userRepo = userRepo;
+            this.matchRepo = matchRepo;
+            this.msgRepo = msgRepo;
+        }
+    }
+}
